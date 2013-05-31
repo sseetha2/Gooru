df8e0251741de5e79a76769133fc4b96ebe31369
Surabhi Seetharam <sseetha2@ncsu.edu>
2013-05-31 15:44:30 -0700

    Added Text Flip
    Text Flip
    ->Added in XML
    ->Added in Code
    EditBox added in XML

--- FlipViewerExample/gen/com/example/flipviewerexample/R.java
+++ FlipViewerExample/gen/com/example/flipviewerexample/R.java
@@ -28,10 +28,14 @@ public final class R {
         public static final int secondpage=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int imageView1=0x7f080001;
-        public static final int imageView2=0x7f080002;
-        public static final int viewFlipper=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int editText1=0x7f080004;
+        public static final int myRelativeLayout=0x7f080000;
+        public static final int relativeLayout1=0x7f080002;
+        public static final int relativeLayout2=0x7f080005;
+        public static final int textView1=0x7f080003;
+        public static final int textView2=0x7f080006;
+        public static final int viewFlipper=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- FlipViewerExample/src/com/example/flipviewerexample/Flip3DViewTransitionActivity.java
+++ FlipViewerExample/src/com/example/flipviewerexample/Flip3DViewTransitionActivity.java
@@ -1,15 +1,17 @@
 package com.example.flipviewerexample;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewAnimator;
 
-import com.example.flipviewerexample.AnimationFactory;
 import com.example.flipviewerexample.AnimationFactory.FlipDirection;
-import com.example.flipviewerexample.R;
 
 public class Flip3DViewTransitionActivity extends Activity {
     /** Called when the activity is first created. */
@@ -19,6 +21,19 @@ public class Flip3DViewTransitionActivity extends Activity {
         setContentView(R.layout.activity_main);
        
         final ViewAnimator viewAnimator = (ViewAnimator)this.findViewById(R.id.viewFlipper);
+        final RelativeLayout relativeLayout = (RelativeLayout)this.findViewById(R.id.relativeLayout1);
+        
+        final TextView myTextView = new TextView(this);
+        myTextView.setText("This is added dynamically");
+        myTextView.setTextColor(Color.parseColor("#FF0000"));
+        myTextView.setTextSize(20);
+        relativeLayout.addView(myTextView);
+        
+        /*final TextView myTextView1 = new TextView(this);
+        myTextView1.setText("This is also added dynamically");
+        myTextView1.setTextColor(Color.parseColor("#FF0000"));
+        myTextView1.setTextSize(20);
+        relativeLayout.addView(myTextView1);*/
        
         /**
          * Bind a click listener to initiate the flip transitions
@@ -29,13 +44,13 @@ public class Flip3DViewTransitionActivity extends Activity {
                                 // This is all you need to do to 3D flip
                                 AnimationFactory.flipTransition(viewAnimator, FlipDirection.LEFT_RIGHT);
                         }
-               
         });
        
         // The following click listeners are not needed (only here to test that clicks
         // are routed to the correct subview of the view animator).
        
-        this.findViewById(R.id.imageView1).setOnClickListener(new OnClickListener() {
+        //this.findViewById(R.id.imageView1).setOnClickListener(new OnClickListener(){
+        this.findViewById(R.id.relativeLayout1).setOnClickListener(new OnClickListener() {
                         @Override
                         public void onClick(View v) {
                                 // This is all you need to do to 3D flip
@@ -44,15 +59,15 @@ public class Flip3DViewTransitionActivity extends Activity {
                         }
                
         });
-       
-        this.findViewById(R.id.imageView2).setOnClickListener(new OnClickListener() {
+        
+      //this.findViewById(R.id.imageView2).setOnClickListener(new OnClickListener(){
+        this.findViewById(R.id.relativeLayout2).setOnClickListener(new OnClickListener() {
                         @Override
                         public void onClick(View v) {
                                 // This is all you need to do to 3D flip
                                 AnimationFactory.flipTransition(viewAnimator, FlipDirection.LEFT_RIGHT);
                                 Toast.makeText(Flip3DViewTransitionActivity.this, "Side B Touched", Toast.LENGTH_SHORT).show();
                         }
-               
         });
        
        
